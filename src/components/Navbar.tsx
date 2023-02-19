3be612ca54ea3300d3b916a8c529121bea10be9b
Michael Schwieger <git@zekurio.xyz>
2023-02-19 16:12:32 +0100

    more stuff done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,62 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+import logo from "../assets/logo.png";
+
+const NavbarContainer = styled.div`
+  background-color: #000;
+  display: flex;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
+`;
+
+const Logo = styled.img`
+    width: 35px;
+    height: 35px;
+    margin: 10px;
+`;
+
+const NavList = styled.ul`
+  list-style: none;
+  display: flex;
+`;
+
+const NavItem = styled.li`
+  display: inline-block;
+  font-family: "Fira Mono", monospace;
+  font-weight: 500;
+`;
+
+const NavLink = styled(Link)`
+  color: #fff;
+  text-decoration: none;
+  font-size: 18px;
+  margin-right: 50px;
+`;
 
 function Navbar() {
-    return (
-        <div className="navbar">
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-                <Link to="/projects">Projects</Link>
-            </li>
-        </div>
-    );
+  return (
+    <NavbarContainer>
+        <Logo src={logo} alt="logo" />
+      <NavList>
+        <NavItem>
+          <NavLink to="/">Home</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink to="/projects">Projects</NavLink>
+        </NavItem>
+        <NavItem>
+            <NavLink to="/setup">Setup</NavLink>
+        </NavItem>
+        <NavItem>
+            <NavLink to="/about">About</NavLink>
+        </NavItem>
+      </NavList>
+    </NavbarContainer>
+  );
 }
 
 export default Navbar;
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import background from "../assets/background2.jpg";
+import logo from "../assets/logo.png";
+
+const HomeHeader = styled.header`
+  width: 100%;
+  height: 100vh;
+  background-image: url(${background});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  transition: transform 0.25s ease-in-out;
+`;
+
+const GrettingContainer = styled.div`
+  padding: 15vh 0;
+  text-align: center;
+  h1 {
+    font-weight: 300;
+    font-size: 80px;
+    font-family: "Fira Sans", sans-serif;
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+  h2 {
+    font-weight: 200;
+    font-size: 27px;
+    font-family: "Fira Sans", sans-serif;
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+`;
 
 function Home() {
   return (
-    <div className="home">
-        <h1>Test</h1>
-    </div>
+    <HomeHeader>
+      <GrettingContainer>
+        <img src={logo} alt="logo" width={300} height={300}/>
+        <h1>Hi 👋</h1>
+        <h2>welcome to my homepage</h2>
+      </GrettingContainer>
+    </HomeHeader>
   );
 }
 
