d9459fd7d8e71f4b34e6c3ed7cdc8f4e38927eee
Michael Schwieger <git@zekurio.xyz>
2023-02-19 17:58:37 +0100

    about me started

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -14,34 +14,63 @@ const HomeHeader = styled.header`
   transition: transform 0.25s ease-in-out;
 `;
 
-const GrettingContainer = styled.div`
+const GreetingContainer = styled.div`
   padding: 15vh 0;
   text-align: center;
   h1 {
     font-weight: 300;
     font-size: 80px;
     font-family: "Fira Sans", sans-serif;
-    margin-top: 0;
     margin-bottom: 0;
   }
-  h2 {
-    font-weight: 200;
-    font-size: 27px;
+`;
+
+const AboutContainer = styled.div`
+  padding: 10vh 0;
+  background-color: #000;
+  h1 {
+    text-align: center;
     font-family: "Fira Sans", sans-serif;
-    margin-top: 0;
-    margin-bottom: 0;
+    font-weight: 200;
+    font-size: 48px;
+  }
+  p {
+    font-family: "Fira Sans", sans-serif;
+    font-weight: 400;
+    font-size: 20px;
+    margin-left: 20vw;
+    margin-right: 20vw;
   }
 `;
 
 function Home() {
   return (
-    <HomeHeader>
-      <GrettingContainer>
-        <img src={logo} alt="logo" width={300} height={300}/>
-        <h1>Hi 👋</h1>
-        <h2>welcome to my homepage</h2>
-      </GrettingContainer>
-    </HomeHeader>
+    <>
+      <HomeHeader>
+        <GreetingContainer>
+          <img src={logo} alt="logo" width={300} height={300}/>
+          <h1>zekurio.xyz</h1>
+        </GreetingContainer>
+      </HomeHeader>
+      <AboutContainer>
+        <h1>ABOUT ME</h1>
+        <p>
+          Hey there! 👋
+          <br />
+          <br />
+          My name is Michael and I'm a software developer from Vienna, Austria. I am currently {new Date().getFullYear() - 2001} years old and I'm studying computer science at the Vienna University of Technology.
+          I have been passionate about programming since I was 14 years old, starting with some basic Batch scripts and other useless stuff. From there, I started to learn Python and Java, which I still sometimes use today.
+          Later on I started to learn Go, Javascript and Typescript, which is what I used to create this website. In addition to that, I also collected some experience with C and C++ while using Linux as a daily driver for some time.
+          <br />
+          <br />
+          My current focus is backend development, with my main programming language being Go. [daemon](https://github.com/zekurio/daemon) is a project that started my journey into Go and I'm still working on it to this day.
+          With daemon I also dipped my toes into containerization and databases, with Docker and Postgresql being my current go-to tools.
+          <br />
+          <br />
+          But I am also an avid PC enthusiast and love tinkering with my hardware, with overclocking being my current nemesis. Of course, I also love to play games, with my current favorites being Cyberpunk 2077 and Valorant.
+        </p>
+      </AboutContainer>
+    </>
   );
 }
 
