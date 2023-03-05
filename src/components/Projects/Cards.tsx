244bdc4746a0be1c1a2ff6a20617e89f5ad64ae7
Michael Schwieger <git@zekurio.xyz>
2023-03-05 01:34:38 +0100

    some changes

--- src/components/Projects/Cards.tsx
+++ src/components/Projects/Cards.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import ProjectCard from "./ProjectCard";
 
-// TODO make them clickable
 const CardsContainer = styled.div`
   display: flex;
   justify-content: center;
